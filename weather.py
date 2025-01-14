733ea00b0d887bdf737ad3b61c00ecd479017149
toribird_wee <159395714+Mianbao514@users.noreply.github.com>
2025-01-14 11:58:10 -0500

    Update weather.py
    cheap

--- weather.py
+++ weather.py
@@ -1,7 +1,11 @@
 price = 1,000,000
 credit = true
 if credit:
-  down = 0.1 * price
-else:
   final = 0.2 * price
-print(f"Down payment: ${down}")
+  total = price - down
+  
+else:
+  down = 0.1 * price
+  total = price - down
+print("Final price" + total)
+
