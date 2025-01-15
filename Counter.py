7df77c0611131092e771b2ee39379187cec320ab
toribird_wee <159395714+Mianbao514@users.noreply.github.com>
2025-01-15 11:38:56 -0500

    Create Counter.py

---
+++ Counter.py
@@ -0,0 +1,19 @@
+import time
+
+counter = 0
+
+While True:
+    print(counter)
+    counter += 1
+    time.sleep(1)
+    if counter == 20
+        coin = input("Would you like to buy 20 coins with 20 coins? (Y/N): ")
+        if coin.upper == "Y":
+            
+        else:
+            for x in range(1,5)
+            input("Are you sure? It's a big deal! (Y/N)")
+            break
+continue
+ counter = 20
+print("ystem-S purchased 20 coins with your 20 coins for you despite what you might have already")
