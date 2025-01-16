93102a13ba438855b02e68fbddf961956066c213
toribird_wee <159395714+Mianbao514@users.noreply.github.com>
2025-01-16 15:47:48 -0500

    Update Counter.py

--- Counter.py
+++ Counter.py
@@ -4,6 +4,14 @@ counter = 0
 purchased = True
 
 while True:
+    if counter == 100:
+        # Display 100 in bold and stop the program
+        print(f"\033[1m{counter}\033[0m")
+        break
+
+    print(counter, end='\r')  
+    counter += 1
+    time.sleep(0.1)
     print(counter, end='\r')
     counter += 1
     time.sleep(0.1)
@@ -18,9 +26,9 @@ while True:
                 purchased = finalDecision.upper() == "Y"
                 if purchased:
                     break
-        break
 
 if purchased:
     print("Thank you!")
 else:
     print("Altomatic purchased!")
+    print(f"Resuming from {counter}...")
