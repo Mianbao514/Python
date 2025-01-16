c0af3f9638f3bdf8818a17735485f43beb1bef21
toribird_wee <159395714+Mianbao514@users.noreply.github.com>
2025-01-16 15:30:56 -0500

    Update Counter.py
    counter update

--- Counter.py
+++ Counter.py
@@ -1,19 +1,26 @@
 import time
 
 counter = 0
+purchased = True
 
-While True:
-    print(counter)
+while True:
+    print(counter, end='\r')
     counter += 1
-    time.sleep(1)
-    if counter == 20
+    time.sleep(0.1)
+    
+    if counter == 20:
         coin = input("Would you like to buy 20 coins with 20 coins? (Y/N): ")
-        if coin.upper == "Y":
-            
-        else:
-            for x in range(1,5)
-            input("Are you sure? It's a big deal! (Y/N)")
-            break
-continue
- counter = 20
-print("ystem-S purchased 20 coins with your 20 coins for you despite what you might have already")
+        
+        if coin.upper() == "N":
+            purchased = False
+            for x in range(1,5):
+                finalDecision = input("Are you sure? It's a big deal! (Y/N)")
+                purchased = finalDecision.upper() == "Y"
+                if purchased:
+                    break
+        break
+
+if purchased:
+    print("Thank you!")
+else:
+    print("You missed out!")
